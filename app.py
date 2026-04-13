713b6c2214ed9257a34c60b64c4050e34b9a5314
Ideal Robot <theidealrobot@gmail.com>
2026-04-13 09:15:26 -0600

    Commit all changes... Base deployment

--- app.py
+++ app.py
@@ -4,7 +4,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("coming_soon.html")
+    return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+
+@app.route("/how-it-works")
+def how_it_works():
+    return render_template("how_it_works.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+# 👇 keep this for local testing
 if __name__ == "__main__":
     app.run(debug=True)
